9dbb99c8f024c20fdbcabe3ec30808c243ea15d0
Christopher Kaster <me@atomicptr.de>
2022-04-02 10:01:21 +0200

    setup input

---
+++ Constants/InputActions.cs
@@ -0,0 +1,15 @@
+namespace LD50.Constants {
+    public sealed class InputActions {
+        public const string UI_ACCEPT = "ui_accept";
+        public const string UI_CANCEL = "ui_cancel";
+        public const string UI_LEFT = "ui_left";
+        public const string UI_RIGHT = "ui_right";
+        public const string UI_UP = "ui_up";
+        public const string UI_DOWN = "ui_down";
+        public const string MOVE_LEFT = "move_left";
+        public const string MOVE_RIGHT = "move_right";
+        public const string MOVE_UP = "move_up";
+        public const string MOVE_DOWN = "move_down";
+        public const string INTERACT = "interact";
+    }
+}
