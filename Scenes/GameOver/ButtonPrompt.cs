af3cca6f0324b98471ecef3be5ebdba6045f57f3
Christopher Kaster <me@atomicptr.de>
2022-04-07 19:44:51 +0200

    force player to look at game over screen for at least 1s

--- Scenes/GameOver/ButtonPrompt.cs
+++ Scenes/GameOver/ButtonPrompt.cs
@@ -6,6 +6,7 @@ using LD50.Constants;
 namespace LD50.Scenes.GameOver {
     public class ButtonPrompt : NinePatchRect {
         [GetNode("AnimationPlayer")] private AnimationPlayer animationPlayer;
+        [GetNode("Timer")] private Timer timer;
 
         public override void _Ready() {
             GetNodeAttribute.Load(this);
@@ -13,10 +14,18 @@ namespace LD50.Scenes.GameOver {
             animationPlayer.Autoplay = "Pulsate";
             animationPlayer.CurrentAnimation = "Pulsate";
             animationPlayer.Play();
+
+            if (timer != null) {
+                timer.Start();
+            }
         }
 
         public override void _Process(float delta) {
             RegionRect = InputDeviceHandler.InteractIcon.ToRect();
+
+            if (timer != null && !timer.IsStopped()) {
+                return;
+            }
 
             if (Input.IsActionJustPressed(InputActions.INTERACT)) {
                 GetTree().ChangeScene("res://Scenes/Game/Game.tscn");
